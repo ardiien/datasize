e142b1291768ce784242d0c3997d0bc48bdad864
Yaroslav Ovdiienko <yaroslav.ovdiienko@gmail.com>
2026-01-19 20:02:50 +0100

    Update Copyright style

--- core/src/main/kotlin/team/idivision/libs/datasize/DataSizeFormatter.kt
+++ core/src/main/kotlin/team/idivision/libs/datasize/DataSizeFormatter.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright 2026 ardiien
- *  Licensed under the Apache License, Version 2.0.
- *  See http://www.apache.org/licenses/LICENSE-2.0
+ * Licensed under the Apache License, Version 2.0.
+ * See http://www.apache.org/licenses/LICENSE-2.0
  */
 package team.idivision.libs.datasize
 
