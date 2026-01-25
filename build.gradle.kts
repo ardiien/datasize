f976749be392ecfdafe2bafce3cb91a1c4841b6e
Yaroslav Ovdiienko <yaroslav.ovdiienko@gmail.com>
2026-01-25 16:46:28 +0100

    Setup publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,4 +2,6 @@ plugins {
     alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.shadow) apply false
     alias(libs.plugins.bcv) apply false
+    alias(libs.plugins.dokka) apply false
+    alias(libs.plugins.mcentral.publish) apply false
 }
--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,11 +1,19 @@
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.SourcesJar
+
 plugins {
-    `maven-publish`
-    alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.bcv)
+    alias(libs.plugins.dokka)
+    alias(libs.plugins.kotlin.jvm)
+    alias(libs.plugins.mcentral.publish)
 }
 
 group = "io.github.ardiien.datasize"
 version = "1.0.0"
+
+java {
+    withSourcesJar()
+}
 
 kotlin {
     jvmToolchain(17)
@@ -30,11 +38,47 @@ dependencies {
     testImplementation(libs.kotlin.test)
 }
 
-publishing {
-    publications {
-        create<MavenPublication>("maven") {
-            from(components["kotlin"])
-            artifactId = "datasize"
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+
+    configureBasedOnAppliedPlugins(
+        javadocJar = JavadocJar.Dokka("dokkaGenerateHtml"),
+        sourcesJar = SourcesJar.Sources()
+    )
+
+    coordinates(
+        groupId = group.toString(),
+        artifactId = "datasize",
+        version = version.toString(),
+    )
+
+    pom {
+        name.set("datasize")
+        description.set("A lightweight Kotlin library for representing and operating on data sizes")
+        inceptionYear.set("2026")
+        url.set("https://github.com/ardiien/datasize")
+
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("https://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("yovdiienko")
+                name.set("Yaroslav Ovdiienko")
+                email.set("idivision.developer@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:https://github.com/ardiien/datasize.git")
+            developerConnection.set("scm:git:ssh://github.com:ardiien/datasize.git")
+            url.set("https://github.com/ardiien/datasize")
         }
     }
 }
